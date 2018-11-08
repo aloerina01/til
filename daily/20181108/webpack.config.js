add6114d04b9f43095eb27fde7259249452e86f4
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2018-11-08 17:36:42 +0900

    Add webpack condig

---
+++ daily/20181108/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const __MODE__ = process.env.NODE_ENV;
+const __DEV__ = __MODE__ === 'development';
+
+const config = {
+  mode: __MODE__,
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/webpack'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
+
+module.exports = config;
